d22ace3d0e18a3d1d8629d2a401cfb9eb894bf2b
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-13 10:29:22 +0100

    finish

---
+++ snack-1/main.js
@@ -0,0 +1,15 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+const firstNumber = Number(prompt('inserire il primo numero'));
+const secondNumber = Number(prompt('isnerisci il secondo numero'));
+
+if (firstNumber > secondNumber){
+    alert(`${firstNumber} e il maggiore`);
+} else if (secondNumber > firstNumber) {
+    alert(`${secondNumber} e il maggiore`);
+} else {
+    alert('i due numeri sono auguali');
+}
+
+location.reload();
