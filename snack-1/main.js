44e4ec45faacc37d7f0f52e975cbf96b0140ea30
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-13 10:57:01 +0100

    add control input

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,15 +1,20 @@
 /* L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
+
 const firstNumber = Number(prompt('inserire il primo numero'));
 const secondNumber = Number(prompt('isnerisci il secondo numero'));
+console.log(firstNumber, secondNumber);
 
-if (firstNumber > secondNumber){
+if (isNaN(firstNumber) || isNaN(secondNumber)){
+    alert('input non valido');
+} else if (firstNumber > secondNumber) {
     alert(`${firstNumber} e il maggiore`);
 } else if (secondNumber > firstNumber) {
     alert(`${secondNumber} e il maggiore`);
 } else {
     alert('i due numeri sono auguali');
 }
+
 
 location.reload();
