b7e31590dafc338f9d7a8ec1022b029ff9b9a3ae
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-13 12:40:57 +0100

    add input check and condition on result print

--- snack-4/main.js
+++ snack-4/main.js
@@ -3,13 +3,24 @@ Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.*/
 
 const oddNumbers = [];
+let flag = true;
 
 for (let i = 0; i < 6; i++){
     let userNumber = Number(prompt(`inserire il ${i + 1} numero`));
-    if (userNumber % 2 != 0){
+    if(isNaN(userNumber)){
+        alert('inserire solo valori numerici')
+        flag = false;
+        break;
+    } else if (userNumber % 2 != 0){
         oddNumbers[i] = userNumber;
     }
 }
 
 console.log(oddNumbers);
-alert(`i numeri dispari sono ${oddNumbers}`)
+if (flag == true && oddNumbers.length > 0){
+    alert(`i numeri dispari sono ${oddNumbers}`);
+} else if (flag == true && oddNumbers.length == 0) {
+    alert('hai inserito solo valori pari');
+}
+
+location.reload();
